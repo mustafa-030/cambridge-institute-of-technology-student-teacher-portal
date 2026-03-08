40c24c200aaefe8186725a579e48c407cf26afc5
Mustafa Kareem <mustafa.24eee@cambridge.edu.in>
2026-03-08 20:08:35 +0530

    fix root redirect

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
+import os
+from flask import redirect, url_for
 from app import create_app
 
 app = create_app()
-
-from flask import redirect, url_for
 
 @app.route('/')
 def index():
     return redirect(url_for('auth.login'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
