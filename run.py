a6c11428940294292e9fe60f8bf14e9531a2ab7d
Mustafa Kareem <mustafa.24eee@cambridge.edu.in>
2026-03-08 20:04:55 +0530

    add root redirect

--- run.py
+++ run.py
@@ -2,5 +2,11 @@ from app import create_app
 
 app = create_app()
 
+from flask import redirect, url_for
+
+@app.route('/')
+def index():
+    return redirect(url_for('auth.login'))
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
